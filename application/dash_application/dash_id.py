6ad53b70e7615183fffc605a404e091240066bc4
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-10-31 16:44:31 -0400

    Make IDs constant through inspection of stack
    __name__ on its own would always resolve to 'dash_id'. Use inspect
    module to get name of module from which init_ids has been called.

--- application/dash_application/dash_id.py
+++ application/dash_application/dash_id.py
@@ -1,9 +1,14 @@
-import uuid
+from inspect import getmodule, stack
 
-### Helps with graph UUIDs
+### Helps with graph IDs
+## Dash requires unique IDs for the entire app. Reduce mental overhead by 
+## generating IDs with module names appended, effectively making IDs
+## appear local to module.
 
 def init_ids(names):
     results = {}
+    callingModule = getmodule(stack()[1][0])
     for name in names:
-        results[name] = name + '--' + str(uuid.uuid4())
+        ## Replace '.' with '_' because IDs may not have '.' in Dash
+        results[name] = name + '--' + callingModule.__name__.replace('.', '_')
     return results
