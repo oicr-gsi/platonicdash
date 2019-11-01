6d6922656cb223925420b52ff1aea9ae50a46854
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-11-01 11:22:07 -0400

    Add optional param to override module name

--- application/dash_application/dash_id.py
+++ application/dash_application/dash_id.py
@@ -5,10 +5,17 @@ from inspect import getmodule, stack
 ## generating IDs with module names appended, effectively making IDs
 ## appear local to module.
 
-def init_ids(names):
+## Optional parameter allows developer to define name appended to ID
+## rather than automatically getting the name of the calling module.
+## This introduces the danger of the developer setting the same name
+## twice, which will cause Dash to cease correct rendering of pages
+## due to ID conflict. Please use carefully.
+def init_ids(names, moduleName=None):
     results = {}
-    callingModule = getmodule(stack()[1][0])
+    if not moduleName:
+        callingModule = getmodule(stack()[1][0])
+        moduleName = callingModule.__name__
     for name in names:
         ## Replace '.' with '_' because IDs may not have '.' in Dash
-        results[name] = name + '--' + callingModule.__name__.replace('.', '_')
+        results[name] = name + '--' + moduleName.replace('.', '_')
     return results
